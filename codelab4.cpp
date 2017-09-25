239bfaede7e674b49f464d1655cc4bbe8a2d30f6
Ellis Crawford <ellicraw@gmail.com>
2017-09-25 10:04:28 -0400

    finished codelab

--- codelab4.cpp
+++ codelab4.cpp
@@ -6,8 +6,12 @@ using namespace std;
 
 bool is_palindrome(string);
 string Remove_punct(string);
-class ArrayStack
-{
+bool IsOperator(char);
+int PerformOperation(char, int, int);
+bool IsDigit(char);
+int EvaluatePostfix(string);
+
+class ArrayStack {
 private:
     char Arr[MAX_SIZE]; 
     int top;   
@@ -62,8 +66,7 @@ public:
     }
 };
 
-class QueueStack
-{
+class QueueStack {
 private:
 	char Arr[MAX_SIZE];
 	int front, rear; 
@@ -161,6 +164,7 @@ public:
 
 int main()
 {
+	// for testing the stack 
 	// ArrayStack S; // Create instance of ArrayStack
 	// S.Push('o');S.Print();
 	// S.Push('s');S.Print();
@@ -173,6 +177,7 @@ int main()
     // S.Pop(); 
     // S.Len();
 
+	// For testing the queue
     // QueueStack Q; // creating an instance of QueueStack. 
     // Q.Enqueue('b');  Q.Print();  
     // Q.Enqueue('w');  Q.Print();  
@@ -180,26 +185,39 @@ int main()
     // Q.Dequeue();	  Q.Print();
     // Q.Enqueue('a');  Q.Print();
     // Q.Len();
-    string word;
-    bool answer; 
-    cout << "Input a word and I will tell you if it's a palindrome ";
-    cin >> word;
-    answer = is_palindrome(word);
-    if (answer == true) {
-        cout << word << " is a palindrome";
-    } else {
-        cout << word << " is not a palindrome";
-    }
+
+	// for testing the palindrome 
+	// string word;
+    // bool answer; 
+    // cout << endl << "Input a word and I will tell you if it's a palindrome: ";
+    // getline(cin,word);
+    // answer = is_palindrome(word);
+    // if (answer == true) {
+    //     cout << word << " is a palindrome" << endl;
+    // } else {
+    //     cout << word << " is not a palindrome" << endl;
+	// }
+	
+	// for testing the postfix notation
+	string expression; 
+	cout<<"Enter Postfix Expression: ";
+	getline(cin,expression);
+	int result = EvaluatePostfix(expression);
+	cout<<"Output: "<< expression << ", " << result<< endl;
+
 }
 
 bool is_palindrome(string word)
 {
-    ArrayStack s;
+	ArrayStack s;
+	char c;
     int n = word.size();
-
     for(int i = 0; i < n/2; i++)
     {
-        s.Push(word[i]);
+		if (isalpha(word[i]) && !isblank(word[i])) { // if the character is a letter
+			c = word[i];
+			s.Push(tolower(c)); // if character is uppercase, make it lower case
+		}
     }
 
     for(int i = (n+1)/2; i < n; i++)
@@ -216,6 +234,52 @@ bool is_palindrome(string word)
 
 }
 
-string Remove_punct(string word) {
+bool IsOperator(char C)
+{
+	if(C == '+' || C == '-' || C == '*' || C == '/')
+		return true;
 
+	return false;
+}
+
+int PerformOperation(char operation, int operand1, int operand2) {
+	if(operation == '+') return operand1 +operand2;
+	else if(operation == '-') return operand1 - operand2;
+	else if(operation == '*') return operand1 * operand2;
+	else if(operation == '/') return operand1 / operand2;
+
+	else cout<<"Unexpected Error \n";
+	return -1; 
+}
+
+bool IsDigit(char C) {
+	if(C >= '0' && C <= '9') return true;
+	return false;
+}
+
+int EvaluatePostfix(string expression)
+{
+	ArrayStack S;
+
+	for(int i = 0;i< expression.length();i++) {
+		if(expression[i] == ' ' || expression[i] == ',') continue; 
+		else if(IsOperator(expression[i])) {
+			int operand2 = S.Top(); 
+			S.Pop();
+			int operand1 = S.Top(); 
+			S.Pop();
+			int result = PerformOperation(expression[i], operand1, operand2);
+			S.Push(result);
+		}
+		else if(IsDigit(expression[i])){
+			int operand = 0; 
+			while(i<expression.length() && IsDigit(expression[i])) {
+				operand = (operand*10) + (expression[i] - '0'); 
+				i++;
+			}
+			i--;
+			S.Push(operand);
+		}
+	}
+	return S.Top();
 }
