3de0b3f435da7bb52822c81c0255c7fcf31c3d66
Ellis Crawford <ellicraw@gmail.com>
2017-10-16 10:18:23 -0400

    codelab6

--- codelab6.cpp
+++ codelab6.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <locale>
 #include <string>
 using namespace std;
 
-string get_word_to_add(string raw_word, int &max_chars);
-void send_to_output(string line, int);
+string getWordToAdd(string raw_word, int &max_chars);
+void sendToOutput(string line, int);
 
 ifstream instream;
 ofstream outstream;
@@ -26,22 +25,22 @@ while (instream >> temp_word)
 {
 size_of_word = temp_word.size();
 if (size_of_word < max_chars) {
-word = get_word_to_add(temp_word, max_chars);
+word = getWordToAdd(temp_word, max_chars);
 temp_line = temp_line + word;
 }
 else {
-send_to_output(temp_line, margins);
-temp_line = get_word_to_add(temp_word, max_chars);
+sendToOutput(temp_line, margins);
+temp_line = getWordToAdd(temp_word, max_chars);
 max_chars = 80 - margins - temp_line.size();
 }
 }
-send_to_output(temp_line, margins);
+sendToOutput(temp_line, margins);
 }
 outstream.close();
 instream.close();
 return 0;
 }
-string get_word_to_add(string raw_word, int &max_chars)
+string getWordToAdd(string raw_word, int &max_chars)
 {
 string word;
 char a = raw_word.back();
@@ -56,7 +55,7 @@ max_chars = max_chars - (word.size());
 return word;
 }
 
-void send_to_output(string line, int margins) {
+void sendToOutput(string line, int margins) {
 // put margins and print it to console
 margins = margins/2;
 string left_margin(margins,' ');
